8fc762b2d7ca1c29eba0d790acfd84ef97064960
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-17 12:34:32 +0200

    Add css_path tests

---
+++ tests/test_locator_css.py
@@ -0,0 +1,64 @@
+﻿import re
+
+from playwright.async_api import Page
+from playwright_dompath import dom_path_sync
+
+
+def pageFromTemplate(template: str) -> str:
+    return re.sub(
+        r"[\s\n]+/s",
+        " ",
+        f"""data:text/html;charset=utf-8,<!DOCTYPE html>
+            <html>
+                <body>
+                    {template}
+                </body>
+            </html>""",
+    )
+
+
+def test_should_return_correct_path_for_id(page: Page):
+    page.goto(pageFromTemplate("<div id='test'></div>"))
+    div = page.locator("#test")
+    path = dom_path_sync.css_path(div)
+    assert path == "#test"
+
+
+def test_should_return_correct_path_for_single_class(page: Page):
+    page.goto(pageFromTemplate('<div class="test">test</div><div>test2</div>'))
+    div = page.locator(".test")
+    path = dom_path_sync.css_path(div)
+    assert path == "body > div.test"
+
+
+def test_should_return_the_correct_path_for_multiple_classes(page: Page):
+    page.goto(pageFromTemplate('<div class="tst test">test</div><div>test2</div>'))
+    div = page.locator(".test")
+    path = dom_path_sync.css_path(div)
+    assert path == "body > div.tst.test"
+
+
+def test_should_return_the_correct_path_for_nested_classes(page: Page):
+    page.goto(
+        pageFromTemplate(
+            '<div class="main">main<div class="child">child</div><div></div></div>'
+        )
+    )
+    div = page.locator(".main .child")
+    path = dom_path_sync.css_path(div)
+    assert path == "body > div > div.child"
+
+
+def test_should_return_the_correct_path_for_multiple_elements(page: Page):
+    page.goto(pageFromTemplate("<div>test1</div><div>test2</div><div>test3</div>"))
+    divs = page.locator("div")
+    for i in range(divs.count()):
+        path = dom_path_sync.css_path(divs.nth(i))
+        assert path == f"body > div:nth-child({i + 1})"
+
+
+def test_should_return_the_correct_path_for_nested_element(page: Page):
+    page.goto(pageFromTemplate("<div><div>test</div></div>"))
+    div = page.locator("div > div")
+    path = dom_path_sync.css_path(div)
+    assert path == "body > div > div"
